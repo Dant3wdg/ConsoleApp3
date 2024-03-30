5d73cd78b40464515f31733f9b72acb1a0057467
Dante <Dante@DESKTOP-SH607Q8>
2024-03-30 21:13:34 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args);
+            public class Rectangle
+        {
+            public int Xcoord { get; set; }
+            public int Ycoord { get; set; }
+            public int Perimeter()
+            {
+                return 2 * (Math.Abs(Xcoord) + Math.Abs(Ycoord));
+            }
+        }
+    }
+    }
+}
